e974b6da6ab0ef69e7fcbc96a7beb209adc7463c
Jon Kesler <jkesler@hotmail.com>
2020-08-18 21:35:20 -0400

    Create build file

---
+++ precache-manifest.c2e16eaf86a87f98cc46603128511861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1344177fe10f6ed070d1bc5a16fe37",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "7cf0bc8f89278776a81b",
+    "url": "/EmployeeDirectory/static/css/main.27899f2b.chunk.css"
+  },
+  {
+    "revision": "484e424f74bc4d80e4c9",
+    "url": "/EmployeeDirectory/static/js/2.e9d53484.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/EmployeeDirectory/static/js/2.e9d53484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf0bc8f89278776a81b",
+    "url": "/EmployeeDirectory/static/js/main.15c60839.chunk.js"
+  },
+  {
+    "revision": "0379ed1047e17183f6a3",
+    "url": "/EmployeeDirectory/static/js/runtime-main.af9aeaf8.js"
+  }
+]);
